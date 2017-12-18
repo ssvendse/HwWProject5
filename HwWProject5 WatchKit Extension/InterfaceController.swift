cc9b4a3543fdabc1490cbef0d0741b660ec03d5a
Skyler Svendsen <skylersvendsen@gmail.com>
2017-12-17 23:39:56 -0600

    first commit

--- HwWProject5 WatchKit Extension/InterfaceController.swift
+++ HwWProject5 WatchKit Extension/InterfaceController.swift
@@ -8,14 +8,129 @@
 
 import WatchKit
 import Foundation
-
+import UserNotifications
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var tlButton: WKInterfaceButton!
+    @IBOutlet var trButton: WKInterfaceButton!
+    @IBOutlet var blButton: WKInterfaceButton!
+    @IBOutlet var brButton: WKInterfaceButton!
+    
+    var buttons = [WKInterfaceButton]()
+    var startTime = Date()
+    
+    var colors = [
+        "Red": UIColor.red,
+        "Green": UIColor(red: 0, green: 0.5, blue: 0, alpha: 1),
+        "Blue": UIColor.blue,
+        "Orange": UIColor.orange,
+        "Purple": UIColor.purple,
+        "Black": UIColor.black
+    ]
+    
+    var currentLevel = 0
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        buttons += [tlButton, trButton, blButton, brButton]
+        startNewGame()
+        //setPlayReminder()
+    }
+    
+    func buttonTapped(_ button: WKInterfaceButton) {
+        if button == buttons[0] {
+            levelUp()
+        } else {
+            button.setEnabled(false)
+        }
+    }
+    
+    @IBAction func tlButtonTapped() {
+        buttonTapped(tlButton)
+    }
+    
+    @IBAction func trButtonTapped() {
+        buttonTapped(trButton)
+    }
+    
+    @IBAction func blButtonTapped() {
+        buttonTapped(blButton)
+    }
+    
+    @IBAction func brButtonTapped() {
+        buttonTapped(brButton)
+    }
+    
+    func levelUp() {
+        currentLevel += 1
+        
+        if currentLevel == 10 {
+            let playAgain = WKAlertAction(title: "Play Again", style: .default) {
+                self.startNewGame()
+            }
+            
+            let timePassed = Date().timeIntervalSince(startTime)
+            presentAlert(withTitle: "You win!", message: "You finished in \(Int(timePassed)) seconds.", preferredStyle: .alert, actions: [playAgain])
+            return
+        }
+        
+        var colorKeys = Array(colors.keys)
+        colorKeys.shuffle()
+        buttons.shuffle()
+        
+        for (index, button) in buttons.enumerated() {
+            button.setBackgroundColor(colors[colorKeys[index]])
+            
+            button.setEnabled(true)
+            
+            if index == 0 {
+                button.setTitle(colorKeys[colorKeys.count - 1])
+            } else {
+                button.setTitle(colorKeys[index])
+            }
+        }
+    }
+    
+    @IBAction func startNewGame() {
+        startTime = Date()
+        currentLevel = 0
+        levelUp()
+    }
+    
+    func createNotification() {
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "We miss you!"
+        content.body = "Come back and play the game some more!"
+        content.categoryIdentifier = "play_reminder"
+        content.sound = UNNotificationSound.default()
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
+    
+    func setPlayReminder() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { success, error in
+            if success {
+                self.registerCategories()
+                center.removeAllPendingNotificationRequests()
+                self.createNotification()
+            }
+        }
+    }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        
+        let play = UNNotificationAction(identifier: "play", title: "Play Now", options: .foreground)
+        let category = UNNotificationCategory(identifier: "play_reminder", actions: [play], intentIdentifiers: [])
+        
+        center.setNotificationCategories([category])
     }
     
     override func willActivate() {
